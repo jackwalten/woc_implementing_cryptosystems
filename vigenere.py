668e59392e899d855fb75b678f02256daf4061f6
jackwalten <jackwalten@ideapad>
2024-01-14 16:23:53 +0530

    Fixed vigenere logic to handle spaces and other characters

--- vigenere.py
+++ vigenere.py
@@ -13,11 +13,14 @@ def encrypt(plaintxt,key):
     plainLen = len(plaintxt)
     key = adjustkey(key,plainLen)
     ciphertxt = ''
+    j = 0
     for i in range(plainLen):
         if plaintxt[i].islower():
-            ciphertxt += chr((index(plaintxt[i]) + index(key[i].lower())) % 26 + ord('a'))
+            ciphertxt += chr((index(plaintxt[i]) + index(key[j].lower())) % 26 + ord('a'))
+            j += 1
         elif plaintxt[i].isupper():
-            ciphertxt += chr((index(plaintxt[i]) + index(key[i].lower())) % 26 + ord('A'))
+            ciphertxt += chr((index(plaintxt[i]) + index(key[j].upper())) % 26 + ord('A'))
+            j += 1
         else:
             ciphertxt += plaintxt[i]
     return ciphertxt
@@ -26,11 +29,14 @@ def decrypt(ciphertxt,key):
     cipherLen = len(ciphertxt)
     key = adjustkey(key,cipherLen)
     plaintxt = ''
+    j = 0
     for i in range(cipherLen):
         if ciphertxt[i].islower():
-            plaintxt += chr(ord('a') + (ord(ciphertxt[i]) - ord(key[i].lower())) % 26)
+            plaintxt += chr(ord('a') + (ord(ciphertxt[i]) - ord(key[j].lower())) % 26)
+            j += 1
         elif ciphertxt[i].isupper():
             plaintxt += chr(ord('A') + (ord(ciphertxt[i]) - ord(key[i].upper())) % 26)
+            j += 1
         else:
             plaintxt += ciphertxt[i]
     return plaintxt
